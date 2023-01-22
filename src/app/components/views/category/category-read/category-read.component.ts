50bf95110b751bbb2e82239ab7f977a680832a7f
TaylanTorres09 <taylantorres@gmail.com>
2023-01-22 08:04:59 -0300

    Add Table

--- src/app/components/views/category/category-read/category-read.component.ts
+++ src/app/components/views/category/category-read/category-read.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-read.component.css']
 })
 export class CategoryReadComponent {
-
+  displayedColumns: string[] = ['id', 'name', 'description', 'action'];
 }
