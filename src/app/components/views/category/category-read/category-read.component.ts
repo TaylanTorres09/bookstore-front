32bf0507617828613ff344542040a286f8cb17c4
TaylanTorres09 <taylantorres@gmail.com>
2023-01-22 07:36:06 -0300

    create Category component

---
+++ src/app/components/views/category/category-read/category-read.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-read',
+  templateUrl: './category-read.component.html',
+  styleUrls: ['./category-read.component.css']
+})
+export class CategoryReadComponent {
+
+}
