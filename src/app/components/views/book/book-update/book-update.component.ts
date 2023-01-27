9341847b6c70f0a280d6677aa455b4e1b5a96cf4
TaylanTorres09 <taylantorres@gmail.com>
2023-01-27 10:43:04 -0300

    Implement form and ts with method cancel

--- src/app/components/views/book/book-update/book-update.component.ts
+++ src/app/components/views/book/book-update/book-update.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from '../book.model';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-update',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-update.component.css']
 })
 export class BookUpdateComponent {
+
+  book: Book = {
+    title: '',
+    author: '',
+    text: '',
+    categoryId: ''
+  }
+
+  constructor (private service: BookService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    this.book.categoryId = this.route.snapshot.paramMap.get('catId')!;
+  }
+
+  cancel(): void {
+    this.router.navigate([`category/${this.book.categoryId}/book`]);
+  }
 
 }
