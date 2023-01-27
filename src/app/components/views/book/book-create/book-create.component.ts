7304d5cdffdbfcdb885d9213714784a630f9c316
TaylanTorres09 <taylantorres@gmail.com>
2023-01-27 10:10:34 -0300

    Implemented create books

--- src/app/components/views/book/book-create/book-create.component.ts
+++ src/app/components/views/book/book-create/book-create.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Book } from '../book.model';
 import { BookService } from '../book.service';
 
@@ -8,6 +10,7 @@ import { BookService } from '../book.service';
   styleUrls: ['./book-create.component.css']
 })
 export class BookCreateComponent {
+
   book: Book = {
     title: '',
     author: '',
@@ -15,9 +18,22 @@ export class BookCreateComponent {
     categoryId: ''
   }
 
-  constructor () { }
+  constructor (private service: BookService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.book.categoryId = this.route.snapshot.paramMap.get('catId')!;
+  }
+
+  create(): void {
+    this.service.create(this.book).subscribe({
+      next: () => {
+        this.router.navigate([`category/${this.book.categoryId}/book`]);
+        this.service.mesage(`Livro ${this.book.text} criado com sucesso!`);
+      },
+      error: err => {
+        this.service.mesage(err.error.errors[0].message);
+      }
+    });
   }
 
 }
--- src/app/components/views/book/book.service.ts
+++ src/app/components/views/book/book.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { enviroment } from 'src/enviroments/enviroment';
 import { Book } from './book.model';
@@ -11,11 +12,24 @@ export class BookService {
 
   baseUrl: String = enviroment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
   findAllByCategory(catId: String): Observable<Book[]> {
     const url = `${this.baseUrl}/book?category=${catId}`;
     return this.http.get<Book[]>(url);
+  }
+
+  create(book: Book): Observable<Book> {
+    const url = `${this.baseUrl}/book/create`;
+    return this.http.post<Book>(url, book);
+  }
+
+  mesage(str: String): void {
+    this._snack.open(`${str}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000
+    })
   }
 
 }
