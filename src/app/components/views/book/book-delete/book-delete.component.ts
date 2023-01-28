6218a77c3c2e92e28eb357f238f40ed5630efd8f
TaylanTorres09 <taylantorres@gmail.com>
2023-01-28 15:14:11 -0300

    Create delete page, style and functionality

--- src/app/components/views/book/book-delete/book-delete.component.ts
+++ src/app/components/views/book/book-delete/book-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from '../book.model';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-delete',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-delete.component.css']
 })
 export class BookDeleteComponent {
+
+  categoryId: String = '';
+
+  book: Book = {
+    id: '',
+    title: '',
+    author: '',
+    text: '',
+  }
+
+  constructor (private service: BookService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    this.categoryId = this.route.snapshot.paramMap.get('catId')!;
+    this.book.id = this.route.snapshot.paramMap.get('id')!;
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findByIdBook(this.book.id!).subscribe({
+      next: resp => {
+        this.book = resp;
+      },
+      error: err => {
+        this.router.navigate([`category/${this.categoryId}/book`]);
+        console.log(err);
+        this.service.mesage('Erro na identificação do livro');
+      }
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate([`category/${this.categoryId}/book`]);
+  }
 
 }
